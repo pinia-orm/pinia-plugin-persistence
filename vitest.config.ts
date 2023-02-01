48425a84c2441b4c746b5e4b917a760f3c0b9408
elonehoo <hi@elonehoo.me>
2023-02-01 17:27:18 +0800

    chore: init

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'happy-dom',
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'lcov'],
+      src: ['src'],
+    },
+  },
+})
